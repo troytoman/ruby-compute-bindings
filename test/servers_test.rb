94dbce6d7577d50fa33f7c6fbddda859a7316f28
Naveed Massjouni <naveedm9@gmail.com>
2011-08-22 14:44:21 -0400

    Fixing tests.

--- test/servers_test.rb
+++ test/servers_test.rb
@@ -10,51 +10,51 @@ class ServersTest < Test::Unit::TestCase
   
   def test_list_servers
 
-json_response = %{{
-  "servers" : [
-      {
-     "id" : 1234,
-      "name" : "sample-server",
-      "imageRef" : "http://foo.com/v1.1/2",
-      "flavorRef" : "http://foo.com/v1.1/1",
-      "hostId" : "e4d909c290d0fb1ca068ffaddf22cbd0",
-      "status" : "BUILD",
-      "progress" : 60,
-      "addresses" : {
-          "public" : [
-              { "version" : 4, "addr" : "67.23.10.132" },
-              { "version" : 4, "addr" : "67.23.10.131" }
-          ],
-          "private" : [
-              { "version" : 4, "addr" : "10.176.42.16" }
-          ]
-      },
-      "metadata" : {
-          "Server Label" : "Web Head 1",
-          "Image Version" : "2.1"
-      }
-      },
-      {
-    "id" : 5678,
-      "name" : "sample-server2",
-      "imageRef" : "http://foo.com/v1.1/2",
-      "flavorRef" : "http://foo.com/v1.1/1",
-      "hostId" : "9e107d9d372bb6826bd81d3542a419d6",
-      "status" : "ACTIVE",
-      "addresses" : {
-          "public" : [
-              { "version" : 4, "addr" : "67.23.10.133" }
-          ],
-          "private" : [
-              { "version" : 4, "addr" : "10.176.42.17" }
-          ]
-      },
-      "metadata" : {
-          "Server Label" : "DB 1"
-      }
-      }
-  ]
-}}
+    json_response = %{{
+      "servers" : [
+        {
+          "id" : 1234,
+          "name" : "sample-server",
+          "image" : { "id": "2" },
+          "flavor" : { "id" : "1" },
+          "hostId" : "e4d909c290d0fb1ca068ffaddf22cbd0",
+          "status" : "BUILD",
+          "progress" : 60,
+          "addresses" : {
+              "public" : [
+                  { "version" : 4, "addr" : "67.23.10.132" },
+                  { "version" : 4, "addr" : "67.23.10.131" }
+              ],
+              "private" : [
+                  { "version" : 4, "addr" : "10.176.42.16" }
+              ]
+          },
+          "metadata" : {
+              "Server Label" : "Web Head 1",
+              "Image Version" : "2.1"
+          }
+        },
+        {
+          "id" : 5678,
+          "name" : "sample-server2",
+          "image" : { "id": "2" },
+          "flavor" : { "id" : "1" },
+          "hostId" : "9e107d9d372bb6826bd81d3542a419d6",
+          "status" : "ACTIVE",
+          "addresses" : {
+              "public" : [
+                  { "version" : 4, "addr" : "67.23.10.133" }
+              ],
+              "private" : [
+                  { "version" : 4, "addr" : "10.176.42.17" }
+              ]
+          },
+          "metadata" : {
+              "Server Label" : "DB 1"
+          }
+        }
+      ]
+    }}
     response = mock()
     response.stubs(:code => "200", :body => json_response)
 
@@ -71,8 +71,8 @@ json_response = %{{
 
     server=get_test_server
     assert_equal "sample-server", server.name
-    assert_equal "http://foo.com/v1.1/2", server.imageRef
-    assert_equal "http://foo.com/v1.1/1", server.flavorRef
+    assert_equal "2", server.imageId
+    assert_equal "1", server.flavorId
     assert_equal "e4d909c290d0fb1ca068ffaddf22cbd0", server.hostId
     assert_equal "BUILD", server.status
     assert_equal 60, server.progress
@@ -85,30 +85,30 @@ json_response = %{{
 private
   def get_test_server
 
-json_response = %{{
-  "server" : {
-      "id" : 1234,
-      "name" : "sample-server",
-      "imageRef" : "http://foo.com/v1.1/2",
-      "flavorRef" : "http://foo.com/v1.1/1",
-      "hostId" : "e4d909c290d0fb1ca068ffaddf22cbd0",
-      "status" : "BUILD",
-      "progress" : 60,
-      "addresses" : {
-          "public" : [
-              { "version" : 4, "addr" : "67.23.10.132" },
-              { "version" : 4, "addr" : "67.23.10.131" }
-          ],
-          "private" : [
-              { "version" : 4, "addr" : "10.176.42.16" }
-          ]
-      },
-      "metadata" : {
-          "Server Label" : "Web Head 1",
-          "Image Version" : "2.1"
+    json_response = %{{
+      "server" : {
+          "id" : 1234,
+          "name" : "sample-server",
+          "image" : { "id": "2" },
+          "flavor" : { "id" : "1" },
+          "hostId" : "e4d909c290d0fb1ca068ffaddf22cbd0",
+          "status" : "BUILD",
+          "progress" : 60,
+          "addresses" : {
+              "public" : [
+                  { "version" : 4, "addr" : "67.23.10.132" },
+                  { "version" : 4, "addr" : "67.23.10.131" }
+              ],
+              "private" : [
+                  { "version" : 4, "addr" : "10.176.42.16" }
+              ]
+          },
+          "metadata" : {
+              "Server Label" : "Web Head 1",
+              "Image Version" : "2.1"
+          }
       }
-  }
-}}
+    }}
 
     response = mock()
     response.stubs(:code => "200", :body => json_response)
